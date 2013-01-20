093675b463f6abcc10119b68e9408ad2fd6a857c
Sebastian Sito <sebastian@hypenode.com>
2013-01-20 17:59:54 +0100

    Fixes.

--- lib/appmake/listeners/coffee.rb
+++ lib/appmake/listeners/coffee.rb
@@ -22,10 +22,14 @@ module Appmake
 					name = f.split("/").last
 					new_name = name.gsub "coffee", "js"
 
+					puts "=== f: #{f}"
+					puts "=== name: #{name}"
+					puts "=== new_name: #{new_name}"
+
 					system "./node_modules/.bin/coffee -c js/#{name} js/#{new_name}"
 
 					if name[0] == name[0].upcase
-						system "./node_modules/.bin/webmake js/#{name} public/js/#{name}"
+						system "./node_modules/.bin/webmake js/#{new_name} public/js/#{new_name}"
 					end
 				end
 			end
